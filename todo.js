56fb8e08081f500b57b180efee72c82ecc859491
Naveen <naveen281298kumar@gmail.com>
2021-10-17 12:26:56 +0530

    Adding all the file to repository

---
+++ todo.js
@@ -0,0 +1,82 @@
+let add=document.getElementById("add-to-list");
+function getAndUpdate() {
+    console.log("add value");
+     let title=document.getElementById('title').value;
+     let desc=document.getElementById('description').value;
+    if (localStorage.getItem('itemsJson')==null) {
+        let itemJson=[];
+        itemJson.push([title,desc]);
+        localStorage.setItem('itemsJson',JSON.stringify(itemJson));
+    }
+    else{
+         itemJsonstr=localStorage.getItem('itemsJson');
+        itemJson=JSON.parse(itemJsonstr);
+        itemJson.push([title,desc]);
+        localStorage.setItem('itemsJson',JSON.stringify(itemJson));
+    }
+    update();
+}
+
+
+
+ function update() {
+    if (localStorage.getItem('itemsJson')==null) {
+         itemJson=[];
+        localStorage.setItem('itemsJson',JSON.stringify(itemJson));
+    }
+    else{
+         itemJsonstr=localStorage.getItem('itemsJson');
+        itemJson=JSON.parse(itemJsonstr);
+
+    }
+
+// Display value in table
+   let tabelbody=document.getElementById("table-body");
+   let str="";
+    itemJson.forEach((element,index) => {
+        str += `
+        <tr>
+        <th scope="row">${index+1}</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button type="button" class="btn btn-primary btn-sm" onclick="deleted(${index})">Delete</button></td>
+    </tr> `;
+    });
+    tabelbody.innerHTML=str;
+}
+
+add.addEventListener("click",getAndUpdate);
+update();
+
+function deleted(itemIndex) {
+    console.log("Delete",itemIndex);
+
+
+//delete item from table
+    itemJsonstr=localStorage.getItem('itemsJson');
+    itemJson=JSON.parse(itemJsonstr);
+
+    itemJson.splice(itemIndex,1);
+    localStorage.setItem('itemsJson',JSON.stringify(itemJson));
+    update();
+}
+
+
+
+function clearStore() {
+    if (confirm("Do you really want to delete everthing")) {
+        console.log("Clearing storage");
+        localStorage.clear();
+        update(); 
+    }
+}
+
+function clickit() {
+    let c=document.getElementById("clickit");
+    if(c.className==="collapse"){
+        c.className += "responisve"
+    }
+    else{
+        c.className="collaspe";
+    }
+}
